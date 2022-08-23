20604576988c4d583fcf0b72ca259bc542ecb544
Hector <monkhectorsanchez@gmail.com>
2022-08-23 00:50:33 -0400

    added the functionality and edited the HTML and CSS files

--- 3D_Landing_Page/script.js
+++ 3D_Landing_Page/script.js
@@ -1,6 +1,19 @@
 const boxes = document.querySelectorAll('.box')
+const heading = document.querySelector('.heading span')
+const btn = document.querySelector('.btn')
+
+const colors = ['#efd81d', '#61dbfb', '#41b883', '#b52e31', '#43853d', '#cf649a', '#e04e39']
+
+const techs = ['JS', 'React', 'Vue', 'Angular', 'Node', 'Sass', 'Ember']
 
 let current = 1
+
+const textStyle = () => {
+  heading.style.color = colors[current - 1]
+  heading.textContent = techs[current - 1]
+  btn.style.backgroundColor = colors[current - 1]
+  btn.firstElementChild.textContent = techs[current - 1]
+}
 
 let interval = setInterval(() => {
   boxes.forEach((box) => {
@@ -12,5 +25,21 @@ let interval = setInterval(() => {
       box.classList.remove('active')
     }
   })
+  textStyle()
   current++
-}, 1000)
+}, 5000)
+
+boxes.forEach((box) => {
+  box.addEventListener('click', () => {
+    boxes.forEach((cube) => {
+      cube.classList.remove('active')
+    })
+    box.classList.add('active')
+
+    current = box.classList[1].split('-')[1] * 1
+
+    textStyle()
+
+    clearInterval(interval)
+  })
+})
