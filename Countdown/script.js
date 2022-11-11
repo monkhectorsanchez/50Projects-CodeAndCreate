cd02d8ea8773956d5ba3210a27ff77eae7f2bc84
Hector <monkhectorsanchez@gmail.com>
2022-11-10 19:40:07 -0500

    added part of the functionality

---
+++ Countdown/script.js
@@ -0,0 +1,22 @@
+const countdown = document.querySelector('.countdown')
+
+const interval = setInterval(() => {
+  const deadline = new Date(2023, 0, 1, 12, 00, 00)
+
+  const current = new Date()
+
+  const diff = deadline - current
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24)
+  const minutes = Math.floor((diff / (1000 * 60)) % 60)
+  const seconds = Math.floor((diff / (1000)) % 60)
+
+  countdown.innerHTML = `
+    <div>${days}</div>
+    <div>${hours}</div>
+    <div>${minutes}</div>
+    <div>${seconds}</div>
+  `
+}, 1000)
+
