2bce00694626984d0dd716e4a71cf06859bd9a56
Hector <monkhectorsanchez@gmail.com>
2022-11-17 20:14:24 -0500

    added some more funtionality

--- Forms_with_Validation/script.js
+++ Forms_with_Validation/script.js
@@ -3,10 +3,19 @@ const headingSpan2 = document.querySelector('.heading-span-2')
 
 document.querySelector('.signup-btn').addEventListener('click', () => {
   container.classList.add('change')
-  headingSpan2.textContent = 'Up'
+  setTimeout(() => {
+    headingSpan2.textContent = 'Up'
+  }, 200)
 })
 
 document.querySelector('.signin-btn').addEventListener('click', () => {
   container.classList.remove('change')
-  headingSpan2.textContent = 'In'
+  setTimeout(() => {
+    headingSpan2.textContent = 'In'
+  }, 200)
 })
+
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const password2 = document.getElementById('password2')
