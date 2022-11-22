d0c5857b3a722a41d9910cd776e03d766b628084
Hector <monkhectorsanchez@gmail.com>
2022-11-21 20:43:13 -0500

    finish the functionality and turned off the autocomplete

--- Forms_with_Validation/script.js
+++ Forms_with_Validation/script.js
@@ -2,12 +2,20 @@ const container = document.querySelector('.container')
 const headingSpan2 = document.querySelector('.heading-span-2')
 const form = document.querySelector('.form')
 
+const clearForm = () => {
+  document.querySelectorAll('.form-input-wrapper').forEach((item) => {
+    item.className = 'form-input-wrapper'
+  })
+  form.reset()
+}
+
 document.querySelector('.signup-btn').addEventListener('click', () => {
   container.classList.add('change')
   setTimeout(() => {
     headingSpan2.textContent = 'Up'
   }, 200)
   form.className = 'form sign-up'
+  clearForm()
 })
 
 document.querySelector('.signin-btn').addEventListener('click', () => {
@@ -16,6 +24,7 @@ document.querySelector('.signin-btn').addEventListener('click', () => {
     headingSpan2.textContent = 'In'
   }, 200)
   form.className = 'form sign-in'
+  clearForm()
 })
 
 const username = document.getElementById('username')
@@ -34,6 +43,16 @@ const success = (input) => {
   inputWrapper.className = 'form-input-wrapper success'
 }
 
+const checkEmail = (input) => {
+  const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  
+  if(regEx.test(input.value.trim())) {
+    success(input)
+  } else {
+    error(input, 'Email is not valid')
+  }
+}
+
 const checkRequiredFields = (inputArr) => {
   inputArr.forEach((input) => {
     if(input.value.trim() === '') {
@@ -48,12 +67,32 @@ const checkRequiredFields = (inputArr) => {
   })
 }
 
+const checkLength = (input, min, max) => {
+  if(input.value.length < min) {
+    error(input, `${input.id} must be at least ${min} characters`)
+  } else if(input.value.length > max) {
+    error(input, `${input.id} must be less than ${max} characters`)
+  } else {
+    success(input)
+  }
+}
+
+const passwordsMatch = (input1, input2) => {
+  if(input1.value !== input2.value) {
+    error(input2, 'Passwords do not match')
+  }
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
 
   if(form.classList[1] === 'sign-up') {
     checkRequiredFields([username, email, password, password2])
+    checkLength(username, 2, 15)
+    checkLength(password, 5, 25)
+    passwordsMatch(password, password2)
   } else {
     checkRequiredFields([email, password])
   }
+  checkEmail(email)
 })
