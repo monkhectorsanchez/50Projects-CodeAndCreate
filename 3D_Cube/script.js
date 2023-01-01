ce34822cdc99ebdbc69c6870944e309351b7acd8
Hector <monkhectorsanchez@gmail.com>
2022-12-31 20:14:36 -0500

    finished the functionality of both arrows

--- 3D_Cube/script.js
+++ 3D_Cube/script.js
@@ -1,8 +1,41 @@
 let x = 0
 
-document.querySelector('.left-arrow').addEventListener('click', () => {
-  x += 90
+const rotate = () => {
   const cubes = document.querySelectorAll('.cube')
   Array.from(cubes).forEach(cube => cube.style.transform = `rotateY(${x}deg)`)
+}
+
+document.querySelector('.left-arrow').addEventListener('click', () => {
+  x += 90
+  rotate()
+
+})
+
+document.querySelector('.left-arrow').addEventListener('mouseover', () => {
+  x += 25
+  rotate()
+
+})
+
+document.querySelector('.left-arrow').addEventListener('mouseout', () => {
+  x -= 25
+  rotate()
+
+})
+
+document.querySelector('.right-arrow').addEventListener('click', () => {
+  x -= 90
+  rotate()
+})
+
+document.querySelector('.right-arrow').addEventListener('mouseover', () => {
+  x -= 25
+  rotate()
+
+})
+
+document.querySelector('.right-arrow').addEventListener('mouseout', () => {
+  x += 25
+  rotate()
 
 })
